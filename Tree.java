4a8bc00b92ec6e75de65a894541d84ef1398a25d
zacharynarcotta <zachary.narcotta@maine.edu>
2025-02-10 20:48:42 -0500

    Initialize all files; complete Tree class w/ constructor & variables.

---
+++ Tree.java
@@ -0,0 +1,11 @@
+public class Tree {
+    public int ID;
+    public int age;
+    public String species;
+
+    public Tree(int ID, int age, String species) {
+        this.ID = ID;
+        this.age = age;
+        this.species = species;
+    }
+}
