1047b78bfe051eb7a00573d748910445cd586ba8
zacharynarcotta <zachary.narcotta@maine.edu>
2025-02-10 20:59:42 -0500

    Modify Grove class; add planting tree method, add uproot/remove tree method, override toString method.

--- Grove.java
+++ Grove.java
@@ -1,3 +1,34 @@
 public class Grove {
-    
+    public Tree groupTrees[] = new Tree[16];
+    public String groveName;
+
+    public Grove(String groveName) {
+        this.groveName = groveName;
+    }
+
+    public int plantTree(Tree tree) {
+        for(int i = 0; i < groupTrees.length; i++) {
+            if(groupTrees[i] == null) {
+                groupTrees[i] = tree;
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public Tree uprootTree(int location) {
+        Tree uproot = groupTrees[location];
+        groupTrees[location] = null;
+        return uproot;
+    }
+
+    public String toString() {
+        String toPrint = "";
+        for(int i = 0; i < groupTrees.length; i++) {
+            if(groupTrees[i] != null) {
+                toPrint += groupTrees[i].toString();
+            }
+        }
+        return toPrint;
+    }
 }
